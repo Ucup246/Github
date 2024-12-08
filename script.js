a62b3627f144d70346fda377cafaaf6404b7c74f
Ucup246 <muhysuf246@gmail.com>
2024-12-08 11:37:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let nama = 'yusuf'
+let usia = 16
+let tinggiBadan = 165
+let pacar = 'single'
+
+saldoAwal = 3000
+saldoTambahan = 3000
+const hutang = 1000
+const hasil = saldoAwal + saldoTambahan - hutang
+
+let orang = ['yusuf', 'ficky', 'farel', 'rein', 'prema', 'eca', 'farras', 'dede', 'vandy',] //array
+
+alert(`nama saya ${nama} usia saya ${usia} tinggi saya ${tinggiBadan} dan saya ${pacar}`)
+alert (
+    `saldo saya ${saldoAwal} saldo tambahan${saldoTambahan} dikurang hutang ${hutang} maka hasilnya ${hasil}`
+)
+alert(orang[1]) //array
+prompt('nama anda? ') //prompt pop up yg harus diisi oleh orang yg mengakses nya
